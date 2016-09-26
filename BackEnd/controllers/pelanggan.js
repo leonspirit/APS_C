8d481f85068e967ea2c36b36fe110bfe0ac0f9df
Arianto Wibowo <arianto@Ariantos-MacBook-Pro.local>
2016-09-26 12:38:24 +0700

    Pelanggan controller

--- BackEnd/controllers/pelanggan.js
+++ BackEnd/controllers/pelanggan.js
@@ -3,53 +3,98 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 
 var connection = require('../database')
+var token_auth = require('../token')
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.post('/tambah_pelanggan', function(req,res){
 
-    var querystring = 'INSERT INTO pelanggan SET nama = ?, telp = ?, alamat = ?';
-    var pelanggan = [req.body.nama, req.body.telp, req.body.alamat];
-    connection.query(querystring, pelanggan, function(err,result){
-        if(err) throw err;
-        var resp = {pelangganID:result.insertId};
-        res.type('application/json');
-        res.status(200).send(resp);
-    });
+    var resp = {}
+    res.type('application/json');
+    token_auth.check_token(req.body.token, function(result){
+        if(result == null){
+            resp['token_status'] = 'failed'
+            res.status(200).send(resp)
+        }
+        else{
+            resp['token_status'] = 'success'
+            var querystring = 'INSERT INTO pelanggan SET nama = ?, telp = ?, alamat = ?';
+            var pelanggan = [req.body.nama, req.body.telp, req.body.alamat];
+            connection.query(querystring, pelanggan, function(err,result){
+                if(err) throw err;
+                resp['pelangganID'] = result.insertId;
+                res.status(200).send(resp);
+            });
+        }
+    })
 });
 
-router.get('/list_pelanggan', function(req,res){
+router.post('/list_pelanggan', function(req,res){
 
-    var querystring = 'SELECT * FROM pelanggan';
-    connection.query(querystring, function(err, result){
-        if(err) throw err;
-        res.status(200).send(result);
-    });
+    var resp = {}
+    res.type('application/json')
+    token_auth.check_token(req.body.token, function(result){
+        if(result == null){
+            resp['token_status'] = 'failed'
+            res.status(200).send(resp)
+        }
+        else{
+            resp['token_status'] = 'success'
+            var querystring = 'SELECT * FROM pelanggan';
+            connection.query(querystring, function(err, result){
+                if(err) throw err;
+                resp['data'] = result
+                res.status(200).send(resp);
+            });
+        }
+    })
 });
 
 router.post('/hapus_pelanggan', function(req,res){
 
-    var querystring = 'DELETE FROM pelanggan WHERE pelangganID = ?';
-    var pelanggan = [req.body.pelangganID];
-    connection.query(querystring, pelanggan, function(err, result){
-        if(err) throw err;
-        var resp = {affectedRows:result.affectedRows};
-        res.type('application/json');
-        res.status(200).send(resp);
-    });
+    var resp = {}
+    res.type('application/json')
+
+    token_auth.check_token(req.body.token, function(result){
+        if(result == null){
+            resp['token_status'] = 'failed'
+            res.status(200).send(resp)
+        }
+        else{
+            resp['token_status'] = 'success'
+            var querystring = 'DELETE FROM pelanggan WHERE pelangganID = ?';
+            var pelanggan = [req.body.pelangganID];
+            connection.query(querystring, pelanggan, function(err, result){
+                if(err) throw err;
+                resp['affectedRows'] = result.affectedRows
+                res.status(200).send(resp);
+            });
+        }
+    })
 });
 
 router.post('/update_pelanggan', function(req,res){
 
-    var querystring = 'UPDATE pelanggan SET nama = ?, telp = ?, alamat = ? WHERE pelangganID = ?';
-    var pelanggan = [req.body.nama, req.body.telp, req.body.alamat, req.body.pelangganID];
-    connection.query(querystring, pelanggan, function(err, result){
-        if(err) throw err;
-        var resp = {affectedRows:result.affectedRows};
-        res.type('application/json');
-        res.status(200).send(resp);
-    });
+    var resp = {}
+    res.type('application/json')
+
+    token_auth.check_token(req.body.token, function(result){
+        if(result == null){
+            resp['token_status'] = 'failed'
+            res.status(200).send(resp)
+        }
+        else{
+            resp['token_status'] = 'success'
+            var querystring = 'UPDATE pelanggan SET nama = ?, telp = ?, alamat = ? WHERE pelangganID = ?';
+            var pelanggan = [req.body.nama, req.body.telp, req.body.alamat, req.body.pelangganID];
+            connection.query(querystring, pelanggan, function(err, result){
+                if(err) throw err;
+                resp['affectedRows'] = result.affectedRows
+                res.status(200).send(resp);
+            });
+        }
+    })
 });
 
 module.exports = router
